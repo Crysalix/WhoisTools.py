c3318a749f4dfeaee826e36b40797667fe430459
Crysalix <yannick013@gmail.com>
2022-08-26 11:07:13 +0200

    Added 1st arg reading for domains to query

--- whoistools.py
+++ whoistools.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
 import ip2whois
 
 import config as cfg
 
+try:
+    arg1 = sys.argv[1]
+except IndexError:
+    sys.exit('Usage : {} <domain.tld>'.format(sys.argv[0]))
+
 ip2whois_init = ip2whois.Api(cfg.apikey)
 
 # Lookup domain information
-results = ip2whois_init.lookup('timestamp.ovh')
+results = ip2whois_init.lookup(arg1)
 print(results)
